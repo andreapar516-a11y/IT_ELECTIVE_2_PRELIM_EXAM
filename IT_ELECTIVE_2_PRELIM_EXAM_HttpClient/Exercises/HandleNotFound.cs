1ba6b00d7c4be5e0e0a11965d56bda7bbb82a01e
andreapar516-a11y <andreapar516@gmail.com>
2026-07-11 09:25:59 +0800

    handlenotfound

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 9: GET Handle 404 Not Found
@@ -14,13 +17,20 @@ namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class HandleNotFound
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=999999
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert that "meals" field is null (not found)
+        // ? Correct URL
+        string url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=999999";
+        var response = await client.GetAsync(url);
 
-        throw new NotImplementedException();
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception($"Expected 200 OK, got {response.StatusCode}");
+
+        string json = await response.Content.ReadAsStringAsync();
+        using var doc = JsonDocument.Parse(json);
+        var meals = doc.RootElement.GetProperty("meals");
+
+        if (meals.ValueKind != JsonValueKind.Null)
+            throw new Exception("Expected 'meals' field to be null for non-existent ID");
     }
 }
