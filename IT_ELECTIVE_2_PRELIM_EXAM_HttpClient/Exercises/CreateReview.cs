95cdfde66fcd2a42a4b4b9af7ead04bea0e80ec3
andreapar516-a11y <andreapar516@gmail.com>
2026-07-11 09:23:51 +0800

    Createreview

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
@@ -1,29 +1,29 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-// EXERCISE 6: POST Create Review
-// JSONPlaceholder API: https://jsonplaceholder.typicode.com/posts
-//
-// Your task:
-// 1. Create a JSON body string: { "title": "Great Pasta!", "body": "Loved this recipe", "userId": 1 }
-// 2. Wrap it in StringContent with media type "application/json"
-// 3. Send a POST request to the URL
-// 4. Assert status code is 201 Created
-// 5. Parse the response JSON and assert it contains an "id" field
-//
-// Hint: Use await client.PostAsync(url, content)
-// Hint: Use new StringContent(json, Encoding.UTF8, "application/json")
+namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class CreateReview
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public static async Task Run(HttpClient client)
     {
-        // TODO: Create JSON string with title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send POST request to https://jsonplaceholder.typicode.com/posts
-        // TODO: Assert status code is 201 Created
-        // TODO: Parse the response JSON
-        // TODO: Assert the response has an "id" field with a value
+        string url = "https://jsonplaceholder.typicode.com/posts";
+        string jsonBody = @"{""title"":""Great Pasta"",""body"":""Loved this recipe"",""userId"":1}";
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
 
-        throw new NotImplementedException();
+        var response = await client.PostAsync(url, content);
+
+        if (response.StatusCode != HttpStatusCode.Created)
+            throw new Exception($"Expected 201 Created, got {response.StatusCode}");
+
+        string responseJson = await response.Content.ReadAsStringAsync();
+        using var doc = JsonDocument.Parse(responseJson);
+
+        if (!doc.RootElement.TryGetProperty("id", out _))
+            throw new Exception("Response does not contain 'id' field");
     }
 }
